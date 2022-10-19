767aa3167c8365adb36772694b0ce3bb2547978b
Twan Reijinga <43951307+Twan-Reijinga@users.noreply.github.com>
2022-10-19 19:27:01 +0200

    responsive title + move *.css to /styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React, { FC } from 'react';
-import './App.css';
+import './styles/App.css';
 import HighlitedText from './components/HighlightedText';
 
 const App: FC = () => {
   return (
     <div className="App">
       <HighlitedText> Hello world! </HighlitedText>
+      <h1 className="sub-title">
+        I'm Twan<span>.</span>
+      </h1>
     </div>
   );
 };
