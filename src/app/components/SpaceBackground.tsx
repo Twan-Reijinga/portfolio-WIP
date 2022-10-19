9ecb9fe0f1c5d80c0ddbbf6f1c6106d986680fca
Twan Reijinga <43951307+Twan-Reijinga@users.noreply.github.com>
2022-10-19 22:26:45 +0200

    get star positions from json file

--- src/app/components/SpaceBackground.tsx
+++ src/app/components/SpaceBackground.tsx
@@ -1,5 +1,6 @@
 import React, { FC } from 'react';
 import Star from './Star';
+import starPositions from '../config/star-positions.json';
 
 interface starProps {
   top: Number;
@@ -7,68 +8,7 @@ interface starProps {
   size: Number;
 }
 
-const stars: Array<starProps> = [
-  {
-    top: 20,
-    left: 5,
-    size: 7,
-  },
-  {
-    top: 10,
-    left: 20,
-    size: 5,
-  },
-  {
-    top: 12,
-    left: 40,
-    size: 8,
-  },
-  {
-    top: 15,
-    left: 60,
-    size: 8,
-  },
-  {
-    top: 40,
-    left: 65,
-    size: 4,
-  },
-  {
-    top: 45,
-    left: 85,
-    size: 6,
-  },
-  {
-    top: 13,
-    left: 90,
-    size: 5,
-  },
-  {
-    top: 65,
-    left: 20,
-    size: 5,
-  },
-  {
-    top: 90,
-    left: 10,
-    size: 4,
-  },
-  {
-    top: 80,
-    left: 55,
-    size: 6,
-  },
-  {
-    top: 70,
-    left: 70,
-    size: 3,
-  },
-  {
-    top: 60,
-    left: 95,
-    size: 4,
-  },
-];
+const stars: Array<starProps> = starPositions.stars;
 
 const HighlitedText: FC = () => {
   return (
